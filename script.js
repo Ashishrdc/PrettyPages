6f5fa9c267052b3a5207b7abec891136eb01cab1
Ashish Chaurasiya <78536243+Ashishrdc@users.noreply.github.com>
2023-07-04 23:02:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,34 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
+const observerTop = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+
+            entry.target.classList.add('top-anime');
+        } else{
+            entry.target.classList.remove('top-anime');
+        }
+    });
+});
+
+const observerBot = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+
+            entry.target.classList.add('bottom-anime');
+        } else{
+            entry.target.classList.remove('bottom-anime');
+        }
+    });
+});
+
 const hiddenElements = document.querySelectorAll('.hidden');
+const topHidden = document.querySelectorAll('.top-hid');
+const botHidden = document.querySelectorAll('.bot-hid');
+
+topHidden.forEach((e) => observerTop.observe(e));
+botHidden.forEach((e) => observerBot.observe(e));
 hiddenElements.forEach((el) => observer.observe(el));
